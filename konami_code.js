d6fb4e489f2c4837503714585bbe037146e14811
Ronald DeBeras <rdeberas@gmail.com>
2018-04-04 12:42:19 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -5,9 +5,9 @@ function init() {
 
    document.body.addEventListener('keydown', function(e){
     var key = parseInt(e.detail || e.which);
-    if (key === code[index]) {
+    if (key === alphabet[index]) {
       index++;
-      if (index === code.length - 1) {
+      if (index === alphabet.length - 1) {
 
         index = 0;
         alert("Dreamteam!");
